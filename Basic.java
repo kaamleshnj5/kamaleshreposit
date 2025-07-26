5a8f991b5dd2b5d9cfe6c0c5f63aba5425bd4194
Kamalesh Marimuthu <kamaleshnj259@gmail.com>
2025-07-26 16:22:46 +0530

    A Basic Java Program
    This is a simple Java program that takes user input for name, age, and department, then displays the entered values.
    It's useful for beginners learning basic input/output in Java using the Scanner class.

---
+++ Basic.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+import java.util.Scanner;
+
+class Basic {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        String Name = scan.nextLine();
+        int Age = scan.nextInt();
+        String Department = scan.nextLine();
+        System.out.println("My Name is " + Name);
+        System.out.println("My Age is " + Age);
+        System.out.println("My Department is " + Department);
+    }
+}
